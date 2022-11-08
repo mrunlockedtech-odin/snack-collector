ac4e2dfa64780a161b1b2e83539357286942fedc
mrunlockedtech <jrbhrn@gmail.com>
2022-11-07 21:43:55 -0600

    Add index view for snacks

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('snacks/', views.snacks_index, name='snacks_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+class Snack:
+  def __init__(self, name, type, description):
+    self.name = name
+    self.type = type
+    self.description = description
+
+snacks = [
+  Snack('Doritos', 'Chips', 'Dust is really tasty, nice crisp')
+]
+
+def snacks_index(request):
+  return render(request, 'snacks/index.html', {'snacks': snacks})
+
 def home(request):
-  return HttpResponse('<h1>Hello Snackers</h1>')
+  return render(request, 'home.html')
 def about(request):
   return render(request,'about.html')
